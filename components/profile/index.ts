f1ab8ebe3f7f6c2d9c835d58536551539f46b931
Steven <stevenmb1995@gmail.com>
2025-07-12 16:37:36 -0500

    Added profile index.ts

---
+++ components/profile/index.ts
@@ -0,0 +1,6 @@
+export { default as ProfileAvatar } from "./ProfileAvatar";
+export { default as ProfileHeader } from "./ProfileHeader";
+export { default as PersonalInfoCard } from "./PersonalInfoCard";
+export { default as AccountStatsCard } from "./AccountStatsCard";
+export { default as QuickActionsCard } from "./QuickActionsCard";
+export * from "./types";
