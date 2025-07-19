58a7aa97c2c42c3c7490564f18dd50d10386e544
Steven <stevenmb1995@gmail.com>
2025-07-18 21:10:24 -0500

    Fixing linting issues for deployment again

--- components/itemCard/subcomponents/RightContainer.tsx
+++ components/itemCard/subcomponents/RightContainer.tsx
@@ -448,7 +448,7 @@ const PriceContainer: React.FC<Props> = ({
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
             </svg>
             <span className="text-sm text-blue-800">
-              Quantity changed from {qtyInCart} to {localQuantity}. Click "Update Cart" to save changes.
+              Quantity changed from {qtyInCart} to {localQuantity}. Click &quot;Update Cart&quot; to save changes.
             </span>
           </div>
         </div>
